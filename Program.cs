da0b1fcd4a374e5d6deff609a396573ee23ee712
Sarah Lewin <s.lewin@gso.schule.koeln>
2025-02-22 18:24:32 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Geisterspiel");
+int punkte = 0;
+bool gameover = false;
+Random random = new Random();
+while (gameover == false)
+{
+    Console.WriteLine("Hinter einer Tür verbirgt sich ein Geist.\nWelche Tür wählst du? 1, 2 oder 3?");
+    int tuer = Convert.ToInt32(Console.ReadLine());
+    int geisterTuer = random.Next(1, 4);
+    if (tuer == geisterTuer)
+    {
+        Console.WriteLine("Game over! Hier ist ein Geist!");
+        Console.WriteLine("Deine Punkte: {0}", punkte);
+        gameover = true;
+    }
+    else
+    {
+        Console.WriteLine("Kein Geist gefunden!");
+        punkte = punkte + 1;
+    }
+}
